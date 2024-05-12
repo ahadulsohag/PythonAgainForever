0092bb2796f3d6993879656a434ddc1c2f1da585
Sohag <128306527+ahadulsohag@users.noreply.github.com>
2024-05-12 12:02:02 +0600

    Update simplethings.py

--- simplethings.py
+++ simplethings.py
@@ -1,13 +1,1 @@
 print("Hello World!")
-for i in range(0,10):
-    print("simplethings")
-
-while i==9:
-    print("done for loop")
-    i+=1
-    for j in range(0,2):
-        print("haha")
-
-for k in range(1,4):
-    for l in range(k,4):
-        print("*")
