38343e679fda5e2896684cea2326056f2937496e
Sohag <128306527+ahadulsohag@users.noreply.github.com>
2024-05-23 15:13:40 +0600

    Update lists.py

--- lists.py
+++ lists.py
@@ -4,4 +4,10 @@ alist1 = list()
 print(alist1)
 alist = [1, True, "banana"]
 print(alist)
-
+print(alist[-3])
+for i in alist:
+    print(i)
+if "guava" in alist:
+    print("Yes")
+else:
+    print("No")
