2e812b9ddee5e9a586dbcf9e3ec14d34fc9afa90
Sohag <128306527+ahadulsohag@users.noreply.github.com>
2024-05-25 13:10:04 +0600

    Update dictionaries.py

--- dictionaries.py
+++ dictionaries.py
@@ -1,2 +1,6 @@
-users = {1:"sohag", 2:"dfd"}
-print(1)
+user = {"Name": "Sohag", "Age": 23, "Institute": "SMUCT"}
+print(user)
+user ["Email"] = "aha@gmail.com"
+print(user)
+user2 = dict(dfd = "dfd", kjl = "kjk" )
+print(user2)
