715abfe2d1492534eefa2579628bbd0339628235
Sohag <128306527+ahadulsohag@users.noreply.github.com>
2024-05-29 20:19:27 +0600

    Update allthethings.py

--- allthethings.py
+++ allthethings.py
@@ -1,3 +1,16 @@
 #String = immutable, ordered, text representation
 astring = "Hello World!"
 print(type(astring))
+a = """Hello 
+World
+!"""
+print(a)
+a = """Hello \
+World \
+!"""
+print(a)
+print(a[0])
+print(a[1:7])
+print(a[::2])
+print(a[::-1])
+print(a[7:])
