0681d94454f198bac1dadd6cf9edb96ad37540b0
Sohag <128306527+ahadulsohag@users.noreply.github.com>
2024-09-02 23:23:13 +0600

    Update september.py

--- september.py
+++ september.py
@@ -1,2 +1,12 @@
-newlist = [0]*5
+newlist = [2, 3, 7, 9, 9, 0]
 print(newlist)
+extra = [i*i for i in newlist]
+print(extra)
+atuple = ("cat", "dog")
+print(type(atuple))
+atuple = "man", "woman"
+print(type(atuple))
+atuple = "citizen"
+print(type(atuple))
+atuple = "citizen", 
+print(type(atuple))
