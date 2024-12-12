f5f9a6f96d714ede30b7662eef0d916600122b0c
Sohag <ahadulsohag@gmail.com>
2024-12-12 19:14:51 +0600

    Update december12.py

--- december12.py
+++ december12.py
@@ -4,3 +4,10 @@ print(type(alist))
 blist = ["apple", "banana", 1]
 clist = blist[2]
 print(clist)
+blist.append("a")
+print(blist)
+blist.insert(1, "abc")
+print(blist)
+print(len(blist))
+newlist = [3, 5, 1, 4, 0]
+print(sorted(newlist))
