d7a7ee571d22a49c00cb1d71f8f11faf25238735
Sohag <ahadulsohag@gmail.com>
2024-12-28 17:00:10 +0600

    Update december12.py

--- december12.py
+++ december12.py
@@ -1,15 +1,8 @@
-alist = list()
+alist = [1, 2, 3, 4, 5]
+blist = alist[:]
+blist.append(6)
+print(blist)
 print(alist)
-print(type(alist))
-blist = ["apple", "banana", 1]
-clist = blist[2]
-print(clist)
-blist.append("a")
-print(blist)
-blist.insert(1, "abc")
-print(blist)
-print(len(blist))
-newlist = [3, 5, 1, 4, 0]
-print(sorted(newlist))
-atuple = ("d", 1)
-print(type(atuple))
+B = [1, 2, 3, 4, 5]
+C = [i*i for i in B]
+print(C)
