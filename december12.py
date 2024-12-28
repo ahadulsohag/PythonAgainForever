f2240fb405bbf7bd7b410615bed205760a6224a6
Sohag <ahadulsohag@gmail.com>
2024-12-28 17:13:27 +0600

    Update december12.py

--- december12.py
+++ december12.py
@@ -8,3 +8,11 @@ C = [i*i for i in B]
 print(C)
 atuple = (1,)
 print(type(atuple))
+c = (1, 2, 3, 4, 5)
+d = c[::-1]
+print(d)
+d = [i*i for i in d] 
+print(d)
+i = (1, 2, 3, 4, 5)
+i1, i2, i3, i4, i5 = i
+print(type(i2))
