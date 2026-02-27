e3e3093c4c9fe640e03a56d11b6abbd77fd412e1
Sohag <ahadulsohag@gmail.com>
2026-02-27 13:57:34 +0600

    Update closures.py

--- closures.py
+++ closures.py
@@ -1,1 +1,13 @@
-def count()
+def print_with_memory():
+  record = []
+
+  def inner(data):
+    print(data)
+    record.append(data)
+    print(record)
+  return inner
+
+print_ = print_with_memory()
+
+print_("haha")
+print_("hihi")
