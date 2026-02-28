dbcbe3702aa806672c61fc63bd3c6148720ccf36
Sohag <ahadulsohag@gmail.com>
2026-02-28 16:05:41 +0600

    a

--- l.py
+++ l.py
@@ -1,6 +1,4 @@
-i = 100
-
-#de = [i for i in range(5)]
-res = (i==100)
-print(res)
-""" 0, 1, 200, 300, 400"""
+import modules
+modules.rc()
+print(__name__)
+print(modules.__name__)
---
+++ modules.py
@@ -0,0 +1,6 @@
+import random as rd
+
+def rc():
+  rand = rd.choice("123")
+  print(rand)
+  print("hi")
